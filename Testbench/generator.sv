345382c9dece1133f29863cccfdad57ef4244081
Chandra Prakash <62211521+cp024s@users.noreply.github.com>
2024-04-04 16:22:07 +0530

    Create generator.sv

---
+++ Testbench/generator.sv
@@ -0,0 +1,25 @@
+// ------------------------------------------------------------ CLASS GENERATOR -----------------------------------------------------
+class count_gen;
+        count_trans trans_h;
+        count_trans data2send;
+
+        mailbox #(count_trans)gen2dr;
+
+        function new(mailbox #(count_trans)gen2dr);
+                this.gen2dr  = gen2dr;
+                this.trans_h = new;
+        endfunction: new
+
+        virtual task start();
+            fork
+                begin
+                    for(int i=0; i<no_of_transactions; i++)
+                        begin
+                            assert(trans_h.randomize());
+                            data2send = new trans_h;
+                            gen2dr.put(data2send);
+                        end
+                    end
+           join_none
+        endtask:start
+endclass: count_gen
